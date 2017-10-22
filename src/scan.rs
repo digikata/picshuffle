323039996c1029a0518e80557a4fa1ae725dca25
Alan C <argot@digikata.com>
2017-10-21 23:45:08 -0700

    scan executes

--- src/scan.rs
+++ src/scan.rs
@@ -2,7 +2,32 @@
 
 use ignore::WalkBuilder;
 
-fn scan_path(dir: &str) {
+use crypto::digest::Digest;
+use crypto::sha2::Sha256;
+
+use std::fs::File;
+use std::io::Read;
+
+pub fn hash_file(fname: &str) -> Vec<u8>
+{
+    let mut h = Sha256::new();
+
+
+    let mut buf = [0u8; 4096];
+
+    let mut f = File::open(fname).expect("open file");
+    while let Ok(nbytes) = f.read(&mut buf) {
+        if nbytes == 0 { break; }
+        h.input(&buf[0..nbytes]);
+    }
+
+    let mut out = Vec::with_capacity(h.output_bytes());
+    h.result(&mut out.as_mut_slice());
+    out
+}
+
+
+pub fn scan_path(dir: &str) {
     
     let walk = WalkBuilder::new(dir);
 
@@ -10,31 +35,25 @@ fn scan_path(dir: &str) {
 
     let fsitr = walk.build();
     for res in fsitr {
-        let dirstr = match res {
-            Ok(p) => {
-                if let Some(ft) = p.file_type() {
-                    if ft.is_file() {
-                        let str = p.path().to_str().expect("invalide file path");
-                        String::from(str)
-                    } else {
-                        continue;
-                    }
-                } else {
-                    continue;
+        if let Ok(p) = res {
+            if let Some(ft) = p.file_type() {
+                if ft.is_file() {
+                    let str = p.path().to_str().expect("invalide file path");
+                    vdat.push(String::from(str))
                 }
-            },
-            Err(e) => {
-                println!("{:?}", e);
-                continue;
             }
-        };
-        vdat.push(dirstr);
+        }
     }
     
+    let mut hashes: Vec<(&str, Vec<u8>)> = Vec::new();
     for p in vdat.iter() {
-        println!("{:?}", p);
+        
+        let h = hash_file(p);
+        println!("{:?} {:?}", p, h);
+        hashes.push((p, h));
     }
 }
+
 
 #[cfg(test)]
 mod test {
