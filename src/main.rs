275c324ab36e9b1b365affabefb1fddca02506f8
Alan C <digikata@gmail.com>
2017-10-21 23:45:08 -0700

    scan executes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 
 
 extern crate ignore;
+extern crate crypto;
+extern crate clap;
 
+use clap::{ App, Arg };
 
 mod scan;
+use scan::scan_path;
+
 
 fn main() {
+    let app = App::new("pcoalesce")
+        .version("0.1")
+        .arg(Arg::with_name("dir")
+            .short("d")
+            )
+        ;
 
+    let opts = app.get_matches();
+    if let Some(dir) = opts.value_of("dir") {
+        scan_path(dir);
+    }
 }
